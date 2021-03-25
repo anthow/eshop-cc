27e494e453900f235b52635e71c2434a268b16c0
Anthony Englebert <team@avantconseils.be>
2021-03-25 20:28:50 +0100

    /

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,71 +4,79 @@ import {Layout} from '../components/Layout';
    const ContactPage = () =>{
       return(
           <Layout>
-                 <main className="lg">
-        <h1 className="font-bold text-2xl text-center mb-5">Contact</h1>
+                    <section className="flex flex-col justify-center items-start  flex-wrap md:w-full contact-shadow">
 
-        <div className="contact-form w-full mb-20 w-max m-auto">
+                 <img className="mb-10 mt-10 block w-1/6 m-auto" alt="contact" src="logo-seul.svg"/>
+
+        <div className="contact-form w-full mb-20 w-max m-auto ">
           <form method="post" action="#">
             <div className="form-name m-auto">
-              <label className="sm:inline-block mr-5 mb-3 ">
-                <p>Prénom</p>
+            <label className="block  m-auto sm:inline-block mr-5  mb-3 block">
                 <input
                   type="text"
-                  className="border"
+                  className="border p-1"
                   name="prenom"
                   id="prenom"
+                  placeholder="Prénom"
                 />
               </label>
 
-              <label className="sm:inline-block">
-                <p>nom</p>
-                <input className="border " type="text" name="nom" id="nom" />
+              <label className="  sm:inline-block mr-5  mb-3 block">
+                <input className="border p-1 " placeholder="nom"
+   type="text" name="nom" id="nom" />
               </label>
             </div>
             <div className="form-name">
               <label className=" sm:inline-block mr-5  mb-3 block">
-                <p> Email</p>
                 <input
-                  className="border"
+                  className="border p-1"
                   type="email"
                   name="email"
                   id="email"
+                  placeholder="Email"
+
                 />
               </label>
               <label className="  inline-block mb-3 block">
-                <p>sujet</p>
                 <input
-                  className="border"
+                  className="border p-1"
                   type="text"
                   name="subject"
                   id="subject"
+                  placeholder="Sujet"
+
                 />
               </label>
             </div>
             <label className="block">
-              Message
               <textarea
-                className=" w-max block mb-3 border"
+                className=" w-max block mb-3 border p-2"
                 name="message"
                 id="message"
                 rows="5"
+                cols="42"
+                placeholder="votre message"
+
               />
             </label>
+    
             <button
-              className="  block p-1 w-auto bg-yellow-300 text-center"
-              type="submit"
-            >
-              Envoyer
-            </button>
+                className="  block  p-2  w-auto  text-center valeur rounded-xl text-white "
+                name="button"
+                type="button"
+              >
+                Envoyer
+              </button>
           </form>
         </div>
-        <section className="flex justify-center items-start  flex-wrap md:w-full">
-          <article className=" md:w-5/12 mb-5 md:mb-0 mx-5 w-10/12">
-            <h2 className="font-bold text-2xl text-left  mb-5">
+        </section>
+        <section className="flex justify-center items-start  flex-wrap md:w-full  mb-10">
+          <article className="m-auto  flex flex-col md:w-5/12 mb-5 md:mb-0 mx-5 w-10/12">
+            <h2 className="font-bold text-2xl text-center  mb-5">
               {" "}
               Information de contact
             </h2>
-            <h3 className="font-bold text-xl text-left mb-2">
+            <h3 className="font-bold text-xl text-center mb-2">
               {" "}
               Nom de l'entreprise
             </h3>
@@ -81,36 +89,41 @@ import {Layout} from '../components/Layout';
             <p>numéro de compte en banque</p>
           </article>
           <article className=" md:w-5/12 mb-0 mx-5 w-10/12">
-            <h2 className="font-bold text-2xl text-left mb-5">
+            <h2 className="font-bold text-2xl text-center mb-5">
           
               réseaux sociaux
             </h2>
+            <div className="flex items-center justify-center mb-10">
+            <img className=" block w-40" alt="Coccinelles et compagnies" src="logo-titres.svg"/>
             <a href="https://www.facebook.com">
-              <svg className="inline-block mr-3"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z" />
-              </svg>
-              </a>
-              <a href="https://www.instagram.com/">
+            <img className=" block w-8" alt="facebook" src="facebook.svg"/>
 
-              <svg className="inline-block"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-              </svg>
-            </a>
+              </a>
+
+            </div>
+
+            <div className="flex items-center justify-evenly">
+            <div className=" flex flex-col">
+            <img className=" block w-20 mb-2" alt="Coccinelles et compagnies" src="laine.svg"/>
+            <a href="https://www.facebook.com">
+            <img className=" block w-6" alt="facebook" src="facebook.svg"/>
+
+              </a>
+              </div>
+              <div className=" flex flex-col">
+            <img className=" block w-20 mb-2" alt="Coccinelles et compagnies" src="ygaelle.svg"/>
+            <a href="https://www.facebook.com">
+            <img className=" block w-6" alt="facebook" src="facebook.svg"/>
+
+              </a>
+              </div>
+            </div>
+
           </article>
         </section>
-      </main>
           </Layout>
       )
   }
 
   export default ContactPage;
+ 

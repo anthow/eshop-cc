892071e7a3125fcc801a4c4b4d85bb742ef81c0d
Anthony Englebert <anthonyenglebert@siriuscoop.be>
2021-05-07 12:45:25 +0200

    Update index.js

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -12,6 +12,6 @@ export default () =>
         <meta property="og:url" content="https://coccinellesetcompagnie.com/" />
         <meta property="og:title" content="Coccinelles et compagnie" />
         <meta property="og:description" content="Coccinelles et compagnie, boutique/ateliers" />
-        <meta property="og:image" content="/https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />        
+        <meta property="og:image" content="atelier-boutique.png" />        
     </Helmet>
 

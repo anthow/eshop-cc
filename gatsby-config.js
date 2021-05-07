da2273efaedb881447e72d942a3cb56bf046fc6e
Anthony Englebert <anthonyenglebert@siriuscoop.be>
2021-05-07 11:26:21 +0200

    /

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,19 +101,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.ico`,
-        
-      },
-    },
+   
     {
       resolve: `gatsby-plugin-breadcrumb`,
       options: {
